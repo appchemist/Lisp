981a2d9bd613c52152039505119963b1e35a0eae
appchemist <appchemist@naver.com>
2014-12-17 00:23:23 +0900

    update TextGameEngine

--- LandOfLisp/TextGameEngine.lisp
+++ LandOfLisp/TextGameEngine.lisp
@@ -5,9 +5,6 @@
 			(attic (you are in the attic.
 				there is a giant welding torch in the corner.))))
 
-(defun describe-location (location nodes)
-  (cadr (assoc location nodes)))
-
 (defparameter *edges* '((living-room 
 			 (garden west door)
 			 (attic upstairs ladder))
@@ -16,12 +13,6 @@
 			(attic 
 			 (living-room downstairs ladder))))
 
-(defun describe-path (edge)
-  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
-
-(defun describe-paths (location edges)
-  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
-
 (defparameter *objects* '(whiskey bucket frog chain))
 
 (defparameter *object-locations* '((whiskey living-room)
@@ -29,7 +20,16 @@
 				   (chain garden)
 				   (frog garden)))
 
-(defun object-at (loc objs obj-locs)
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(defun objects-at (loc objs obj-locs)
   (labels ((at-loc-p (obj)
 	     (eq (cadr (assoc obj obj-locs)) loc)))
     (remove-if-not #'at-loc-p objs)))
@@ -38,3 +38,27 @@
   (labels ((describe-obj (obj)
 	     `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-obj (object-at loc objs obj-loc)))))
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+	  (describe-paths *location* *edges*)
+	  (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+		    (cdr (assoc *location* *edges*))
+		    :key #'cadr)))
+    (if next
+	(progn (setf *location* (car next))
+	       (look))
+	'(you cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object
+		 (objects-at *location* *objects* *object-locations*))
+	 (push (list object 'body) *object-locations*)
+	 `(you are now carrying the ,object))
+	(t '(you cannot get that.))))
+
+(defun inventory()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
